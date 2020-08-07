5bf808a6b62f3eca827d231b483e6dca937cef58
Jay Oliver <jaykamas@gmail.com>
2020-08-07 15:58:03 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+class School
+
+    attr_accessor :roster
+
+    def initialize name
+        @name = name
+        @roster = {}
+   end
+
+   def add_student name, grade
+        if @roster[grade] == nil
+            @roster[grade] = []
+        end
+        @roster[grade] << name
+   end
+
+   def grade grade
+    @roster[grade]
+   end
+
+   def sort 
+    @roster.each do |key, value| 
+        value.sort!
+    end
+   end
+
+
+
+end
+
+#sorted_names = @roster.map do |key, value|
+#    value.sort
+#end
