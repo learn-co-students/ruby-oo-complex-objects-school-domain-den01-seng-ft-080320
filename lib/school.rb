bbd8d41d27859f3c54e68b424ce2daef5b9e6fbc
boyloe <boyloe@gmail.com>
2020-08-08 08:21:06 -0600

    Implement add_student method

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+
+class School
+    attr_accessor :name, :roster
+    def initialize  name
+        @name = name
+        @roster = Hash.new
+    end
+    
+    def add_student name, grade
+        @roster[grade] = name 
+    end
+end
