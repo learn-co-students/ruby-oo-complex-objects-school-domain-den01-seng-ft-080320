7cfd1d2624bde281fdfff6a002d1a31ef9823a91
boyloe <boyloe@gmail.com>
2020-08-09 11:49:45 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -7,6 +7,20 @@ class School
     end
     
     def add_student name, grade
-        @roster[grade] = name 
+        roster[grade] ||= []
+        roster[grade] << name
     end
+
+    def grade grade
+        roster[grade]
+    end
+
+    def sort
+        sorted_roster = Hash.new
+         roster.each do |grade, name|
+            sorted_roster[grade] = name.sort
+         end
+         sorted_roster
+    end
+    
 end
